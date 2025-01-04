7c3dce07075a9ec708691f0c649e3ffa5d945a96
abz <akbarkhojayev@gmail.com>
2025-01-04 18:34:22 +0500

    models yangilandi

--- main/admin.py
+++ main/admin.py
@@ -2,9 +2,28 @@ from django.contrib import admin
 from django.contrib.auth.models import Group , User
 from .models import *
 
+class UstozAdmin(admin.ModelAdmin):
+    list_display = ('ism' , 'jins' , 'fan')
+
+    search_fields = ['ism',]
+    search_help_text = "Ism orqali qidiring"
+
+class YonalishAdmin(admin.ModelAdmin):
+    list_display = ('nom',)
+
+    list_filter = ('nom',)
+    search_fields = ['nom',]
+    search_help_text = "Nom orqali qidiring"
+
+class FanAdmin(admin.ModelAdmin):
+    list_display = ('nom',)
+    list_filter = ('yonalish',)
+    search_fields = ['nom',]
+    search_help_text = "Nom orqali qidiring"
+
+admin.site.register(Yonalish , YonalishAdmin)
+admin.site.register(Fan, FanAdmin)
+admin.site.register(Ustoz , UstozAdmin)
+
 admin.site.unregister(Group)
 admin.site.unregister(User)
-
-admin.site.register(Yonalish)
-admin.site.register(Fan)
-admin.site.register(Ustoz)
